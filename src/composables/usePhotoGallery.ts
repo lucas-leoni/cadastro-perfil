414fcd9075fa97ff2b85aca374e30c1e8c48780c
lucas-leoni <lucas.leoni09@gmail.com>
2021-09-30 19:41:31 -0300

    First Commit

---
+++ src/composables/usePhotoGallery.ts
@@ -0,0 +1,119 @@
+import { ref, onMounted, watch } from "vue";
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from "@capacitor/camera";
+import { Filesystem, Directory } from "@capacitor/filesystem";
+import { Storage } from "@capacitor/storage";
+import { isPlatform } from "@ionic/vue";
+import { Capacitor } from "@capacitor/core";
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
+
+export function usePhotoGallery() {
+  const PHOTO_STORAGE = "photos";
+  const photos = ref<UserPhoto[]>([]);
+
+  const cachePhotos = () => {
+    Storage.set({
+      key: PHOTO_STORAGE,
+      value: JSON.stringify(photos.value),
+    });
+  };
+
+  const convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    let base64Data: string;
+    // "hybrid" will detect mobile - iOS or Android
+    if (isPlatform("hybrid")) {
+      const file = await Filesystem.readFile({
+        // eslint-disable-next-line
+        path: photo.path!,
+      });
+      base64Data = file.data;
+    } else {
+      // Fetch the photo, read as a blob, then convert to base64 format
+      // eslint-disable-next-line
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      base64Data = (await convertBlobToBase64(blob)) as string;
+    }
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    if (isPlatform("hybrid")) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath,
+      };
+    }
+  };
+
+  const takePhoto = async () => {
+    const cameraPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+
+    const fileName = new Date().getTime() + ".jpeg";
+    const savedFileImage = await savePicture(cameraPhoto, fileName);
+    /* {
+			filepath: fileName,
+			webviewPath: cameraPhoto.webPath
+		}; */
+
+    photos.value = [savedFileImage, ...photos.value];
+  };
+
+  const loadSaved = async () => {
+    const photoList = await Storage.get({ key: PHOTO_STORAGE });
+    const photosInStorage = photoList.value ? JSON.parse(photoList.value) : [];
+    if (!isPlatform("hybrid")) {
+      for (const photo of photosInStorage) {
+        const file = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+    }
+    photos.value = photosInStorage;
+  };
+
+  onMounted(loadSaved);
+  watch(photos, cachePhotos);
+  return {
+    photos,
+    takePhoto,
+  };
+}
--- src/views/Tab3.vue
+++ src/views/Tab3.vue
@@ -2,27 +2,96 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 3</ion-title>
+        <ion-title>Perfil</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 3</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 3 page" />
+      <div class="container">
+        <div class="bg">
+          <div class="avatar">
+            <ion-fab-button href="/tabs/tab2">
+              <ion-icon :icon="personCircle"></ion-icon>
+            </ion-fab-button>
+          </div>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title>Nome: {{ nome }}</ion-card-title>
+            </ion-card-header>
+          </ion-card>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title> Data de Nascimento: {{ data }} </ion-card-title>
+            </ion-card-header>
+          </ion-card>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-title> Telefone: {{ telefone }} </ion-card-title>
+            </ion-card-header>
+          </ion-card>
+        </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { personCircle } from "ionicons/icons";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonFabButton,
+  IonIcon,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+} from "@ionic/vue";
 
-export default  {
-  name: 'Tab3',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+export default {
+  name: "Tab3",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonFabButton,
+    IonIcon,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+  },
+  setup() {
+    return {
+      personCircle,
+    };
+  },
+};
 </script>
+
+<style scoped>
+.container {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #dfe6e9;
+}
+.bg {
+  padding: 20px;
+  border-radius: 5px;
+  background-color: #b2bec3;
+}
+.avatar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 0px 20px 0px;
+}
+ion-icon {
+  width: 100%;
+  height: 100%;
+}
+</style>
